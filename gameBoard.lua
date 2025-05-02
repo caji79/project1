435fccca26dca84aaacf373aa6961ce5096180b9
Eli Chen <jiananchen0@gmail.com>
2025-05-01 19:34:53 -0700

    foundations stacking is working

--- gameBoard.lua
+++ gameBoard.lua
@@ -1,5 +1,10 @@
 GameBoardClass = {}
 
+GameBoardClass.spadeSymbol = love.graphics.newImage('assets/spade.png')
+GameBoardClass.clubSymbol = love.graphics.newImage('assets/club.png')
+GameBoardClass.heartSymbol = love.graphics.newImage('assets/heart.png')
+GameBoardClass.diamondSymbol = love.graphics.newImage('assets/diamond.png')
+
 function GameBoardClass:new()
     local gameBoard = {}
     local metadata = {__index = GameBoardClass}
@@ -24,6 +29,12 @@ function GameBoardClass:drawGrid()
     love.graphics.rectangle('line', 610, 25, CARD_WIDTH * CARD_SCALE_X, CARD_HEIGHT * CARD_SCALE_Y, 5)
     love.graphics.rectangle('line', 725, 25, CARD_WIDTH * CARD_SCALE_X, CARD_HEIGHT * CARD_SCALE_Y, 5)
     love.graphics.rectangle('line', 840, 25, CARD_WIDTH * CARD_SCALE_X, CARD_HEIGHT * CARD_SCALE_Y, 5)
+
+    -- placeholder suit symbols
+    love.graphics.draw(GameBoardClass.spadeSymbol, 468 + (CARD_WIDTH * CARD_SCALE_X)/2, (CARD_HEIGHT * CARD_SCALE_Y)/2)
+    love.graphics.draw(GameBoardClass.clubSymbol, 585 + (CARD_WIDTH * CARD_SCALE_X)/2, (CARD_HEIGHT * CARD_SCALE_Y)/2)
+    love.graphics.draw(GameBoardClass.heartSymbol, 698 + (CARD_WIDTH * CARD_SCALE_X)/2, (CARD_HEIGHT * CARD_SCALE_Y)/2 + 5)
+    love.graphics.draw(GameBoardClass.diamondSymbol, 813 + (CARD_WIDTH * CARD_SCALE_X)/2, (CARD_HEIGHT * CARD_SCALE_Y)/2 + 3)
 
     -- stock & draw
     love.graphics.rectangle('line', 25, 25, CARD_WIDTH * CARD_SCALE_X, CARD_HEIGHT * CARD_SCALE_Y, 5)
